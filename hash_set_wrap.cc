b1b8716f81b8a39444f4bd6ad6b0f64b3d0fdcba
mailtokartik1 <mailtokartik1@gmail.com>
2019-07-31 16:31:14 +0900

    change export set

--- hash_set_wrap.cc
+++ hash_set_wrap.cc
@@ -18,6 +18,14 @@ using v8::Persistent;
 using v8::String;
 using v8::Boolean;
 using v8::Value;
+using v8::NewStringType;
+
+#if V8_MAJOR_VERSION >= 7
+#define CHECK_SET(X) X.Check()
+#else
+#define CHECK_SET(X) (void)X
+#endif
+
 
 Persistent<Function> HashSetWrap::constructor;
 
@@ -42,8 +50,10 @@ void HashSetWrap::Init(Local<Object> exports) {
 
   constructor.Reset(isolate,
     tpl->GetFunction(isolate->GetCurrentContext()).ToLocalChecked());
-  exports->Set(String::NewFromUtf8(isolate, "HashSet"),
-    tpl->GetFunction(isolate->GetCurrentContext()).ToLocalChecked());
+  CHECK_SET(exports->Set(isolate->GetCurrentContext(), String::NewFromUtf8(isolate, "HashSet", NewStringType::kNormal).ToLocalChecked(),
+  tpl->GetFunction(isolate->GetCurrentContext()).ToLocalChecked()));
+  // exports->Set(String::NewFromUtf8(isolate, "HashSet"),
+  //   tpl->GetFunction(isolate->GetCurrentContext()).ToLocalChecked());
 }
 
 void HashSetWrap::New(const FunctionCallbackInfo<Value>& args) {
